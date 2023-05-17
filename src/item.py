181fde5d260304e56168cb7066b494a903e982c8
Евгений <xarvat@list.ru>
2023-05-17 17:21:05 +0300

    charge func

--- src/item.py
+++ src/item.py
@@ -2,10 +2,10 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
+    pay_rate = 1
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int):
         """
         Создание экземпляра класса item.
 
@@ -13,18 +13,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
-
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        self.calculate_total_price = self.price * self.quantity
+        return self.calculate_total_price
 
-    def apply_discount(self) -> None:
+    def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = int(self.price * self.pay_rate)
+        return self.price
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+def test_main():
+    assert item1.price == 10000
+    assert item1.quantity == 20
+
+
+def test_calculate_total_price():
+    item1.calculate_total_price()
+    assert 10000 * 20 == 200000
+
+
+def test_apply_discount():
+    item2.apply_discount()
+    assert 20000 * Item.pay_rate == 20000
